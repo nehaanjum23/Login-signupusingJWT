739b64e2e47a207147f3fe31691114439cb893e8
Neha <nehaanjum2309@gmail.com>
2023-09-01 18:39:42 -0700

    Fixed issues

--- api/views.py
+++ api/views.py
@@ -16,13 +16,17 @@ class UserRegistrationView(APIView):
 
 class UserLoginView(APIView):
     def post(self, request):
-        serializer = UserLoginSerializer(data=request.data, many=True)
-        if serializer.is_valid(raise_exception = True):
-            email = serializer.data.get('email')
-            password = serializer.data.get('password')
-            user = authenticate(email = email, password = password)
+        serializer = UserLoginSerializer(data=request.data)  
+        if serializer.is_valid(raise_exception=True):
+            email = serializer.validated_data.get('email')  
+            password = serializer.validated_data.get('password')
+            user = authenticate(request, email=email, password=password)  
+
             if user is not None:
-                return Response({'msg' : 'Login Sucessfull'},status=status.HTTP_200_OK)
+               
+                return Response({'msg': 'Login Successful'}, status=status.HTTP_200_OK)
             else:
-                return Response({'msg' : 'error'},status=status.HTTP_400_BAD_REQUEST)
+                
+                return Response({'msg': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
+
 
